c0b56f384547d8d5a2582c0910a5a1d517f43e2a
devAdaid <ahyeong2533@gmail.com>
2023-06-24 17:29:25 +0900

    드래그&드랍까지 구현

---
+++ DesktopFriend/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace DesktopFriend
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.rightClickMenuStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.ExitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.rightClickMenuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // rightClickMenuStrip
+            // 
+            this.rightClickMenuStrip.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.rightClickMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ExitToolStripMenuItem});
+            this.rightClickMenuStrip.Name = "rightClickMenuStrip";
+            this.rightClickMenuStrip.Size = new System.Drawing.Size(241, 69);
+            // 
+            // ExitToolStripMenuItem
+            // 
+            this.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem";
+            this.ExitToolStripMenuItem.Size = new System.Drawing.Size(240, 32);
+            this.ExitToolStripMenuItem.Text = "종료";
+            this.ExitToolStripMenuItem.Click += OnExitClicked;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(422, 308);
+            this.ContextMenuStrip = this.rightClickMenuStrip;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.TopMost = true;
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.rightClickMenuStrip.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        public void OnExitClicked(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private System.Windows.Forms.ContextMenuStrip rightClickMenuStrip;
+        private ToolStripMenuItem ExitToolStripMenuItem;
+    }
+}
+
---
+++ DesktopFriend/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DesktopFriend
+{
+    public partial class Form1 : Form
+    {
+        bool isFormDragging = false;
+        Point oPointClicked;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                isFormDragging = true;
+                oPointClicked = new Point(e.X, e.Y);
+                //tmrDrag.Interval = 110;
+                //tmrDrag.Enabled = true;
+            }
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                isFormDragging = false;
+                //tmrDrag.Enabled = false;
+                //SetBits(petWithClothes[0]);
+            }
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isFormDragging)
+            {
+                var oMoveToPoint = PointToScreen(new Point(e.X, e.Y));
+                oMoveToPoint.Offset(oPointClicked.X * -1, (oPointClicked.Y + SystemInformation.CaptionHeight + SystemInformation.BorderSize.Height) * -1 + 24);
+                Location = oMoveToPoint;
+            }
+        }
+    }
+}
